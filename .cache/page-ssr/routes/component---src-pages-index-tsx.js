dc15c0340d6b53af4115268051e3dac143ab21fe
Benjamin <bmilligan9@outlook.com>
2022-04-18 11:14:03 -0400

    h

---
+++ .cache/page-ssr/routes/component---src-pages-index-tsx.js
@@ -0,0 +1,147 @@
+"use strict";
+exports.id = "component---src-pages-index-tsx";
+exports.ids = ["component---src-pages-index-tsx"];
+exports.modules = {
+
+/***/ "./src/pages/index.tsx":
+/*!*****************************!*\
+  !*** ./src/pages/index.tsx ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _babel_runtime_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/esm/defineProperty */ "./node_modules/@babel/runtime/helpers/esm/defineProperty.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { (0,_babel_runtime_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_0__["default"])(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+
+ // styles
+
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif"
+};
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320
+};
+const headingAccentStyles = {
+  color: "#663399"
+};
+const paragraphStyles = {
+  marginBottom: 48
+};
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4
+};
+const listStyles = {
+  marginBottom: 96,
+  paddingLeft: 0
+};
+const listItemStyles = {
+  fontWeight: 300,
+  fontSize: 24,
+  maxWidth: 560,
+  marginBottom: 30
+};
+const linkStyle = {
+  color: "#8954A8",
+  fontWeight: "bold",
+  fontSize: 16,
+  verticalAlign: "5%"
+};
+
+const docLinkStyle = _objectSpread(_objectSpread({}, linkStyle), {}, {
+  listStyleType: "none",
+  marginBottom: 24
+});
+
+const descriptionStyle = {
+  color: "#232129",
+  fontSize: 14,
+  marginTop: 10,
+  marginBottom: 0,
+  lineHeight: 1.25
+};
+const docLink = {
+  text: "TypeScript Documentation",
+  url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
+  color: "#8954A8"
+};
+const badgeStyle = {
+  color: "#fff",
+  backgroundColor: "#088413",
+  border: "1px solid #088413",
+  fontSize: 11,
+  fontWeight: "bold",
+  letterSpacing: 1,
+  borderRadius: 4,
+  padding: "4px 6px",
+  display: "inline-block",
+  position: "relative",
+  top: -2,
+  marginLeft: 10,
+  lineHeight: 1
+}; // data
+
+const links = [{
+  text: "Tutorial",
+  url: "https://www.gatsbyjs.com/docs/tutorial/",
+  description: "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+  color: "#E95800"
+}, {
+  text: "How to Guides",
+  url: "https://www.gatsbyjs.com/docs/how-to/",
+  description: "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
+  color: "#1099A8"
+}, {
+  text: "Reference Guides",
+  url: "https://www.gatsbyjs.com/docs/reference/",
+  description: "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
+  color: "#BC027F"
+}, {
+  text: "Conceptual Guides",
+  url: "https://www.gatsbyjs.com/docs/conceptual/",
+  description: "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
+  color: "#0D96F2"
+}, {
+  text: "Plugin Library",
+  url: "https://www.gatsbyjs.com/plugins",
+  description: "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
+  color: "#8EB814"
+}, {
+  text: "Build and Host",
+  url: "https://www.gatsbyjs.com/cloud",
+  badge: true,
+  description: "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+  color: "#663399"
+}]; // markup
+
+const IndexPage = () => {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("main", {
+    style: pageStyles
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("title", null, "Home Page"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("h1", {
+    style: headingStyles
+  }, "Main Page ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement("br", null)));
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (IndexPage);
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=component---src-pages-index-tsx.js.map
